59aa40b4276ab129b66972ac34d7aecad76199fd
Lettever <jrafaelmtavares@gmail.com>
2026-02-25 00:25:22 +0000

    update script

--- add2repo.sh
+++ add2repo.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Adds scripts from random places into a repo
-#
+
 repo=~/Programming/script-repo
 mkdir -p "$repo" && cd "$repo"
-ln -sf "$(realpath "$1")" .
+ln -f "$(realpath "$1")" .
 git add . && git commit -m "Add $(basename "$1")" && git push
