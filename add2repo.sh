b9336d33064ea37e29912ace1c7c07ff29800432
Lettever <jrafaelmtavares@gmail.com>
2026-02-25 00:29:40 +0000

    Add downloadImageFromUrl.txt

--- add2repo.sh
+++ add2repo.sh
@@ -2,6 +2,10 @@
 # Adds scripts from random places into a repo
 
 repo=~/Programming/script-repo
+og_dir=$(pwd)
+
 mkdir -p "$repo" && cd "$repo"
-ln -f "$(realpath "$1")" .
+ln "$(realpath "$1")" .
 git add . && git commit -m "Add $(basename "$1")" && git push
+
+cd "$og_dir"
